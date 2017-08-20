cb45a0506565c011c207fb0abab7250eaddd57d2
anghanvaishu <anghanvaishu@github.com>
2017-08-20 09:51:42 +0530

    Movie Module

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
   belongs_to :user
+  has_attached_file :image,
+    :command_path => 'C:\Program Files\ImageMagick-7.0.6-Q16',
+    :styles => { :medium => "x300" },
+    :default_url => "xxxxx",
+    :storage => :s3,
+    :bucket => 'xxxxxx'
+  
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
