4fa4aa0168b9b8b9feeca26819a886448559104f
anghanvaishu <anghanvaishu@github.com>
2017-08-21 09:50:06 +0530

    Add Paperclip for Image Upload

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise', '~> 4.3'
-gem 'paperclip', '~> 5.1'
+gem 'paperclip', '~> 4.2.0'
 
 
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  Paperclip.options[:command_path] = "C:/Program Files/ImageM~1"
+  
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -52,5 +54,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  Paperclip.options[:command_path] = "/usr/bin"
+  
 end
